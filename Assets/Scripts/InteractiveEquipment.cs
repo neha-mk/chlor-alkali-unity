ad22b30967c0c03a1e03bbc7481d961bb3930fc1
sakshi2k <sakshi2k@yahoo.co.in>
2026-03-07 16:13:53 +0100

    added new materials for scene1 & added tooltip manager for hover to get component name

--- Assets/Scripts/InteractiveEquipment.cs
+++ Assets/Scripts/InteractiveEquipment.cs
@@ -1,3 +1,51 @@
+// using UnityEngine;
+
+// public class InteractiveEquipment : MonoBehaviour
+// {
+//     Renderer rend;
+//     Color originalColor;
+
+//     public Texture2D hoverCursor;
+
+//     public Color hoverColor = Color.yellow;
+
+//     EquipmentData data;
+
+//     void Start()
+//     {
+//         rend = GetComponent<Renderer>();
+//         originalColor = rend.material.color;
+
+//         data = GetComponent<EquipmentData>();
+//     }
+
+//     void OnMouseEnter()
+//     {
+//         rend.material.color = hoverColor;
+//         Cursor.SetCursor(hoverCursor, Vector2.zero, CursorMode.Auto);
+//         rend.material.color = hoverColor;
+//     }
+
+//     void OnMouseExit()
+//     {
+//         Debug.Log("OnMouseExit: " + data.equipmentName);
+//         rend.material.color = originalColor;
+//         Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+//         rend.material.color = originalColor;
+//     }
+
+//     void OnMouseDown()
+//     {
+//         Debug.Log("OnMouseDown: " + data.equipmentName);
+//         UIManager.instance.ShowInfo(
+//             data.equipmentName,
+//             data.equipmentDescription
+//         );
+//     }
+// }
+
+
+
 using UnityEngine;
 
 public class InteractiveEquipment : MonoBehaviour
@@ -23,7 +71,12 @@ public class InteractiveEquipment : MonoBehaviour
     {
         rend.material.color = hoverColor;
         Cursor.SetCursor(hoverCursor, Vector2.zero, CursorMode.Auto);
-        rend.material.color = hoverColor;
+
+        // SHOW TOOLTIP
+        if (TooltipManager.Instance != null && data != null)
+        {
+            TooltipManager.Instance.ShowTooltip(data.equipmentName);
+        }
     }
 
     void OnMouseExit()
@@ -31,7 +84,11 @@ public class InteractiveEquipment : MonoBehaviour
         Debug.Log("OnMouseExit: " + data.equipmentName);
         rend.material.color = originalColor;
         Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
-        rend.material.color = originalColor;
+
+        if (TooltipManager.Instance != null)
+        {
+            TooltipManager.Instance.HideTooltip();
+        }
     }
 
     void OnMouseDown()
